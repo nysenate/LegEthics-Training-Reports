2917736b1cb2cae75a6105619c9fbf59f02cb1c0
Nate Frank <nfrank@narcho.nysenate.gov>
2024-10-24 17:03:21 -0400

    fixed query that stopped working when new courses were added

--- utils.php
+++ utils.php
@@ -37,28 +37,36 @@ function get_db_connection($dbcfg)
 function fetch_exam_results($dbcon, $org, $from, $to)
 {
   $sth = $dbcon->prepare("
-    SELECT u.id as userid, u.firstname , u.lastname, u.email,
-           org.data as organization, u.city, office.data as office,
-           location.data as location, phone.data as phone,
+    WITH user_fields AS (
+        SELECT data.userid,
+               MAX(CASE WHEN fieldid = 5 THEN data END) AS location,
+               MAX(CASE WHEN fieldid = 6 THEN data END) AS organization,
+               MAX(CASE WHEN fieldid = 7 THEN data END) AS office,
+               MAX(CASE WHEN fieldid = 8 THEN data END) AS phone
+          FROM mdl_user_info_data data
+      GROUP BY data.userid
+    )
+    SELECT u.id as userid, u.firstname, u.lastname, u.email,
+           uf.organization, u.city, uf.office,
+           uf.location, uf.phone,
            (CASE WHEN u.confirmed = 0 THEN 'No' ELSE 'Yes' END) as confirmed,
+           crs.shortname,
            (CASE WHEN g.completed IS NULL THEN 'Incomplete'
             ELSE from_unixtime(g.completed) END) as completed
-    FROM mdl_user AS u
-    LEFT JOIN mdl_user_info_data org ON (org.userid = u.id)
-    LEFT JOIN mdl_user_info_data office ON (office.userid = u.id)
-    LEFT JOIN mdl_user_info_data phone ON (phone.userid = u.id)
-    LEFT JOIN mdl_user_info_data location ON (location.userid = u.id)
-    LEFT JOIN mdl_lesson_grades g ON (g.userid = u.id)
-    LEFT JOIN mdl_role_assignments ra ON (ra.userid = u.id)
-    WHERE u.id != 1
-    AND ra.roleid = (SELECT id FROM mdl_role WHERE shortname='student')
-    AND org.fieldid = (SELECT id FROM mdl_user_info_field WHERE shortname='Organization')
-    AND office.fieldid = (SELECT id FROM mdl_user_info_field WHERE shortname='Office')
-    AND location.fieldid = (SELECT id FROM mdl_user_info_field WHERE shortname='Location')
-    AND phone.fieldid = (SELECT id FROM mdl_user_info_field WHERE shortname='Phone')
-    AND org.data = ?
-    AND ((g.completed > ? AND g.completed < ?) OR g.completed IS NULL)
-    ORDER BY u.confirmed DESC, g.completed DESC");
+      FROM mdl_lesson_grades g
+      JOIN mdl_lesson l ON (g.lessonid = l.id)
+      JOIN mdl_course crs ON (l.course = crs.id)
+      JOIN mdl_user u ON (u.id = g.userid)
+ LEFT JOIN user_fields uf ON (uf.userid = g.userid)
+      JOIN mdl_role_assignments ra ON (ra.userid = g.userid)
+      JOIN mdl_context ctxt ON (ra.contextid = ctxt.id)
+     WHERE u.id != 1
+       AND ra.roleid = (SELECT id FROM mdl_role WHERE shortname='student')
+       AND ctxt.contextlevel = 50
+       AND ctxt.instanceid = crs.id
+       AND uf.organization = ?
+       AND ((g.completed > ? AND g.completed < ?) OR g.completed IS NULL)
+  ORDER BY u.confirmed DESC, g.completed DESC");
 
   $sth->execute(array($org, $from, $to));
   $res = $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -71,7 +79,7 @@ function generate_csv_file($dir, $org, $res)
   // headers
   $headers = array('Userid', 'Firstname', 'Lastname', 'Email', 'Organization',
                    'City', 'Office', 'Location', 'Phone',
-                   'Confirmed', 'Completed');
+                   'Confirmed', 'Course', 'Completed');
 
   // office specific csv report for attachment
   $filename = $org.'_'.date('Ymd-His').'.csv';
